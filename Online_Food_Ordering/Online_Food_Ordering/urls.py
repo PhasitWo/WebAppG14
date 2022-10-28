11db8e24cecdbf745ca2f57d4e58acd168d4031c
PhasitWo <vrzoearth@gmail.com>
2022-10-28 21:28:30 +0700

    index page

--- Online_Food_Ordering/Online_Food_Ordering/urls.py
+++ Online_Food_Ordering/Online_Food_Ordering/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ordering.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ordering/', include("ordering.urls"))
+    path('ordering/', include("ordering.urls")),
+    path('', index) # test
 ]
