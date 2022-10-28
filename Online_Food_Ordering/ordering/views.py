dae4bbbd390d3a0344e0bc589e699d166da80582
PhasitWo <vrzoearth@gmail.com>
2022-10-28 22:08:24 +0700

    index stylesheet

--- Online_Food_Ordering/ordering/views.py
+++ Online_Food_Ordering/ordering/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+items = [[1, 2, 3],[4,5,6]]
 def index(request):
-    return render(request, "ordering/index.html")
+    return render(request, "ordering/index.html", {"items":items})
