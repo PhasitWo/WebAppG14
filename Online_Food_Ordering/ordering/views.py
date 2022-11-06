b995f96c101c205a4806d406f28ac9f3bacf51bf
PhasitWo <vrzoearth@gmail.com>
2022-11-07 00:10:07 +0700

    add form

--- Online_Food_Ordering/ordering/views.py
+++ Online_Food_Ordering/ordering/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 from .models import Food
-# Create your views here.
 
+# test cart (change to session later)
+cart = [] 
+
+data = Food.objects.all()
 
 def index(request):
-    return render(request, "ordering/index.html", {"foods":Food.objects.all()})
+    return render(request, "ordering/index.html", {"data": data})
+
+def add(request):
+    if request.method == "POST":
+        # some process here
+        #....
+        messages.success(request, 'Item has been added to the cart.')
+    return HttpResponseRedirect(reverse("ordering:index"))
