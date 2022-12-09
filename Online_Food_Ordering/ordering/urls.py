aa99aeaa85fcf979358900987d3a6c9489454f6f
PhasitWo <vrzoearth@gmail.com>
2022-12-09 14:57:18 +0700

    clear_cart and delete item in cart

--- Online_Food_Ordering/ordering/urls.py
+++ Online_Food_Ordering/ordering/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("cart", views.cart, name="cart"),
     path("add", views.add, name="add"),
     path("login", views.login, name ="login"),
-    path("logout", views.logout, name = "logout")
+    path("logout", views.logout, name = "logout"),
+    path("clear_cart", views.clear_cart, name = "clear_cart"),
+    path("delete", views.delete, name = "delete")
 ]
--- Online_Food_Ordering/ordering/views.py
+++ Online_Food_Ordering/ordering/views.py
@@ -7,21 +7,35 @@ from .models import Food, User
 food_data = Food.objects.all()
 
 def index(request):
-    # if not request.user.is_authenticated:
-    #     redirect to login page
     user_data = request.session.get("user_data", "guest")
     if "cart_data" not in request.session:
         request.session["cart_data"] = []
     return render(request, "ordering/index.html", {"data": food_data, "user": user_data})
 
 def cart(request):
-    return render(request, "ordering/cart.html", {"data": request.session["cart_data"]})
+    user_data = request.session.get("user_data", "guest")
+    mockup = [(item[0], item[1], f"{item[2]}x{item[3]}", f"{int(item[2])*float(item[3])}฿") for item in request.session["cart_data"]]
+    return render(request, "ordering/cart.html", {"data": mockup, "user": user_data})
+
+def clear_cart(request):
+    request.session["cart_data"] = []
+    return HttpResponseRedirect(reverse("ordering:cart"))
+
+def delete(request):
+    if request.method == "POST":
+        cart_data = request.session["cart_data"]
+        for index, item in enumerate(cart_data):
+            if item[0] == request.POST["food_id"]:
+                cart_data.pop(index)
+                request.session["cart_data"] = cart_data
+                break
+    return HttpResponseRedirect(reverse("ordering:cart"))
 
 def add(request):
     if request.method == "POST":
-        request.session["cart_data"] += [request.POST["food_name"]] #test
+        request.session["cart_data"] += [(request.POST["food_id"], request.POST["food_name"], request.POST['food_quantity'], request.POST['food_price'])]
         messages.success(request, 'Item has been added to the cart.')
-    return HttpResponseRedirect(reverse("ordering:cart"))
+    return HttpResponseRedirect("/")
 
 def login(request):
     if request.method == "POST":
