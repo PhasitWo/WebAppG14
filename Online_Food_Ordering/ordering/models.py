f6196c48731ca2231ebf2a21b5e1b8510ae9b84f
PhasitWo <vrzoearth@gmail.com>
2022-12-10 18:30:07 +0700

    real food images

--- Online_Food_Ordering/ordering/models.py
+++ Online_Food_Ordering/ordering/models.py
@@ -1,9 +1,7 @@
-import re
 from django.db import models
 # Create your models here.
 
 class Food(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=64)
     image = models.ImageField(upload_to = "images/")
     price = models.FloatField()
